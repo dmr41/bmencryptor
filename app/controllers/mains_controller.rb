3216696ec2e69ab32f566bd45d560556f74cfec9
David Rivers <dmr41.rivers@gmail.com>
2015-06-07 17:09:23 +0200

    Main page for encryption added -> create encryption page added

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,14 +1,19 @@
 class MainsController < ApplicationController
 
 	def index
+		@booly ||= false
 		@main = Main.new
 	end
 
+	# def new
+	# 	@main = Main.new
+	# end
 	def create
-		user_input = params[:main][:input_text]
-		encryption_instance = OneTimePad.new(user_input)
+		@user_input = params[:main][:input_text]
+		encryption_instance = OneTimePad.new(@user_input)
 		encryption_instance.randomize_phrase
 		encryption_instance.encrypt_phrase
+		puts @user_input.length
 		@unencrypted_message = encryption_instance.starting_phrase
 		@single_pad = encryption_instance.one_time_phrase
 		@encrypted_messsage = encryption_instance.encrypted_message
