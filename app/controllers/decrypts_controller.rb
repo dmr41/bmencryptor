578918db39224540a12dc0a85e774db503a195e2
David Rivers <dmr41.rivers@gmail.com>
2015-07-29 19:22:28 +0200

    fixed empty string and mismatched length input errors

--- app/controllers/decrypts_controller.rb
+++ app/controllers/decrypts_controller.rb
@@ -11,10 +11,22 @@ class DecryptsController < ApplicationController
 		one_time_pad = params[:decrypt][:one_time_pad]
 		encrypted_message = params[:decrypt][:encrypted_message]
 		decryption_instance = OneTimePad.new
-		decryption_instance.decrypt_phrase(encrypted_message, one_time_pad)
-		@unencrypted_message = decryption_instance.starting_phrase
-		@single_pad = decryption_instance.one_time_phrase
-		@encrypted_messsage = decryption_instance.encrypted_message
+		puts "#{one_time_pad} + lookat me"
+		if(one_time_pad == "" && encrypted_message == "")
+			not_empty_state = false
+		else
+			not_empty_state = true
+		end
+		if(one_time_pad.length == encrypted_message.length && not_empty_state )
+			decryption_instance.decrypt_phrase(encrypted_message, one_time_pad)
+			@unencrypted_message = decryption_instance.starting_phrase
+			@single_pad = decryption_instance.one_time_phrase
+			@encrypted_messsage = decryption_instance.encrypted_message
+		else
+			@unencrypted_message = "NO MESSAGE DECRYPTED  INPUTS NOT THE SAME LENGTH"
+			@single_pad = "PAD LENGTH DIFFERED FROM ENCRYPTED MESSAGE LENGTH"
+			@encrypted_messsage = "---------------------------------------------"
+		end
 	end
 
 	def decrypt_params
