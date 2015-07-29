9952b3ba68bf07ba915c8efb0c4cd6a780c4a22e
David Rivers <dmr41.rivers@gmail.com>
2015-07-29 20:29:11 +0200

    added validations for decryption

--- app/controllers/decrypts_controller.rb
+++ app/controllers/decrypts_controller.rb
@@ -13,19 +13,17 @@ class DecryptsController < ApplicationController
 		decryption_instance = OneTimePad.new
 		puts "#{one_time_pad} + lookat me"
 		if(one_time_pad == "" && encrypted_message == "")
-			not_empty_state = false
+			@not_empty_state = false
+			@content_size = true
 		else
-			not_empty_state = true
+			@content_size = (one_time_pad.length == encrypted_message.length)
+			@not_empty_state = true
 		end
-		if(one_time_pad.length == encrypted_message.length && not_empty_state )
+		if(@content_size && @not_empty_state )
 			decryption_instance.decrypt_phrase(encrypted_message, one_time_pad)
 			@unencrypted_message = decryption_instance.starting_phrase
 			@single_pad = decryption_instance.one_time_phrase
 			@encrypted_messsage = decryption_instance.encrypted_message
-		else
-			@unencrypted_message = "NO MESSAGE DECRYPTED  INPUTS NOT THE SAME LENGTH"
-			@single_pad = "PAD LENGTH DIFFERED FROM ENCRYPTED MESSAGE LENGTH"
-			@encrypted_messsage = "---------------------------------------------"
 		end
 	end
 
